ec746211bc502ae4993026e2cc0bde1252a1a346
danielbirhanu <danielbir49@gmail.com>
2025-03-10 01:37:47 +0300

    set API routes

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const connectedToDb = require('./database/db')
+const bookRouter = require('./routes/book-routes')
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 connectedToDb();
 
 app.use(express.json())
+app.use('/api/books', bookRouter)
 
 app.listen(PORT, () => (
     console.log(`Server running successfully on Port ${PORT}`)
